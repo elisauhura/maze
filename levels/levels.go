9b72956303a8b9fffe7dd350a0ce4456650f33bf
Vitor Silva <vitor@silva.moe>
2021-01-31 16:00:23 -0300

    Add more levels

--- levels/levels.go
+++ levels/levels.go
@@ -72,6 +72,72 @@ db
 0 0
 `
 
+const level6 string = `mazemap
+4 4
+orxx
+bdor
+xxob
+xxbx
+3
+1 1 g u
+1 1 g r
+2 1 g d
+1 1 u
+0 1
+`
+
+const level7 string = `mazemap
+3 4
+xob
+obx
+dob
+ddb
+4
+1 0 g r
+1 1 g l
+2 2 b l
+0 3 b r
+1 0 l
+2 0
+`
+
+const level8 string = `mazemap
+7 6
+odoodrx
+rrdbobx
+rrododb
+oorxdrx
+bbrxxbx
+xxbxxxx
+7
+2 3 g l
+5 1 g l
+4 3 g r
+1 4 r r
+4 3 l l
+6 2 b l
+5 3 r l
+2 5 u
+5 4
+`
+
+const level9 string = `mazemap
+8 3
+xrrrrrrx
+doooooob
+xbbbbbbx
+7
+1 0 b d
+2 2 b d
+3 0 b d
+4 0 b d
+5 2 b d
+6 0 b d
+0 1 g r
+0 1 r
+7 1
+`
+
 var Levels = [...]Level{
 	{"level 0", level0},
 	{"level 1", level1},
@@ -79,6 +145,10 @@ var Levels = [...]Level{
 	{"level 3", level3},
 	{"level 4", level4},
 	{"level 5", level5},
+	{"level 6", level6},
+	{"level 7", level7},
+	{"level 8", level8},
+	{"level 9", level9},
 }
 
 type Level struct {
