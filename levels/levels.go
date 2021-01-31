b403152aaa497ef4ef0e10cee0eda5c69ea3fc73
Vitor Silva <vitor@silva.moe>
2021-01-31 12:17:53 -0300

    add more levels

--- levels/levels.go
+++ levels/levels.go
@@ -18,6 +18,20 @@ dddb
 `
 
 const level2 string = `mazemap
+3 3
+oor
+oor
+ddb
+4
+1 0 g d
+1 1 g l
+1 1 g r
+1 1 g d
+0 1 r
+1 1
+`
+
+const level3 string = `mazemap
 8 4
 xoorxxxx
 xoorxxxx
@@ -30,10 +44,41 @@ dddddddb
 7 3
 `
 
+const level4 string = `mazemap
+9 3
+dodrxodrx
+xrxodrxob
+xddbxddbx
+2
+2 0 r r
+6 2 l r
+0 0 r
+8 1
+`
+
+const level5 string = `mazemap
+2 7
+rx
+rx
+rx
+or
+rr
+rr
+db
+2
+0 1 b d
+0 3 g d
+1 6
+0 0
+`
+
 var Levels = [...]Level{
 	{"level 0", level0},
 	{"level 1", level1},
 	{"level 2", level2},
+	{"level 3", level3},
+	{"level 4", level4},
+	{"level 5", level5},
 }
 
 type Level struct {
